621c3c70e0dd81dfe393a6f569aa989099ab8daf
giantrun <jayendrans@gmail.com>
2016-04-20 17:43:23 +0530

    added the supercool project

--- Supercool/ViewController.swift
+++ Supercool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var boomlogo: UIImageView!
+    @IBOutlet weak var boombackground:UIImageView!
+    @IBOutlet weak var uncoolbutton:UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Uncooltocool(sender: AnyObject) {
+        boomlogo.hidden=false
+        boombackground.hidden=false
+        uncoolbutton.hidden=true
+    }
 
 }
 
